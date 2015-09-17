bd2a4df94e76e4d8577ed25c5f8244e1f197496f
adrianmorelosakendos <adrian.morelos@akendos.com>
2015-09-17 10:27:31 -0500

    CC other email addresses on emails bound to the admin email address.

--- wp-content/themes/wpex-elegant/header.php
+++ wp-content/themes/wpex-elegant/header.php
@@ -27,6 +27,17 @@ $logo_img_collapse = get_theme_mod('wpex_logo_collapse');
                 Typekit.load();
             } catch (e) {
             }</script>
+        <!-- Hotjar Tracking Code for admissionsrevolution.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:76969,hjsv:5};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
