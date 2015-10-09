e2643fe5340b7326c44deb50d8d8f84fc82eda63
Maicol Lopez Mora <you@example.com>
2015-10-09 15:38:57 -0500

    featured in, new logos

--- wp-content/themes/wpex-elegant/templates/featured_in.php
+++ wp-content/themes/wpex-elegant/templates/featured_in.php
@@ -9,24 +9,32 @@
             </div>
         </div>
         <div class="row init-row">
-            <div class="symple-column symple-one-fifth symple-column-first   symple-all">
+            <div class="symple-column symple-one-seventh symple-column-first   symple-all">
                 <a href="http://www.latimes.com/" target="_blank"><img class="aligncenter wp-image-1204 size-full" src="http://admissionsrevolution.com/wp-content/uploads/2015/06/LosAngelesTimes.png" alt="LosAngelesTimes" width="136" height="105" /></a></p>
             </div>
 
-            <div class="symple-column symple-one-fifth symple-column-middle   symple-all">
+            <div class="symple-column symple-one-seventh symple-column-middle   symple-all">
                 <a href="http://www.nbcnews.com/" target="_blank"><img class=" size-full wp-image-1205 aligncenter" src="http://admissionsrevolution.com/wp-content/uploads/2015/06/NBC_News.png" alt="NBC_News" width="129" height="105" /></a></p><div class='mailmunch-forms-in-post-middle' style='display: none !important;'></div>
             </div>
 
-            <div class="symple-column symple-one-fifth symple-column-middle   symple-all">
+            <div class="symple-column symple-one-seventh symple-column-middle   symple-all">
                 <a href="http://www.chicagotribune.com/" target="_blank"><img class="aligncenter wp-image-1203 size-full" src="http://admissionsrevolution.com/wp-content/uploads/2015/06/ChicagoTribune.png" alt="ChicagoTribune" width="158" height="108" /></a></p>
             </div>
 
-            <div class="symple-column symple-one-fifth symple-column-middle   symple-all">
+            <div class="symple-column symple-one-seventh symple-column-middle   symple-all">
                 <a href="http://www.businessinsider.com/" target="_blank"><img class=" size-full wp-image-1202 aligncenter" src="http://admissionsrevolution.com/wp-content/uploads/2015/06/BusinessInsider.png" alt="BusinessInsider" width="212" height="86" /></a></p>
             </div>
 
-            <div class="symple-column symple-one-fifth symple-column-last   symple-all">
+            <div class="symple-column symple-one-seventh symple-column-middle   symple-all">
                 <a href="http://abc27.com/" target="_blank"><img class=" size-full wp-image-1201 aligncenter" src="http://admissionsrevolution.com/wp-content/uploads/2015/06/ABC27_News.png" alt="ABC27_News" width="105" height="105" /></a></p>
+            </div>
+
+            <div class="symple-column symple-one-seventh symple-column-middle   symple-all">
+                <a href="http://www.huffingtonpost.com/" target="_blank"><img class=" size-full wp-image-1206 aligncenter" src="http://admissionsrevolution.com/wp-content/uploads/2015/10/huff-post.png" alt="Huff Post" width="105" height="105" /></a></p>
+            </div>
+
+            <div class="symple-column symple-one-seventh symple-column-last   symple-all">
+                <a href="http://community.today.com/parentingteam" target="_blank"><img class=" size-full wp-image-1207 aligncenter" src="http://admissionsrevolution.com/wp-content/uploads/2015/10/badgeExternal_contributor.png" alt="ABC27_News" width="140" height="105" /></a></p>
             </div>
         </div>
     </div>
